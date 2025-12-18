c54a5c977c57d0e57526da3ff1d8bd8100eae6c4
storythurston166-ui <story.thurston166@students.cmcss.net>
2025-12-18 13:51:02 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,31 @@
+#Given variables
+party_pizza_mini = 14
+large = 8
+medium = 6
+people = 6 # friends
 
+#Do Not use crtl+A to select code.  Only copy the code below this line.
+#------------------------------------------------------------------------------------------------
+slices = party_pizza_mini + large + medium
+print(f"slices: {slices}")
+
+people = 12
+share = 28 // 12
+leftover = share
+print(f"share_result")
+print(f"leftover_result")
+ people = people + Eric + Brandon
+
+share = 28 // 14
+leftover = 0
+print(f"share_results")
+print(f"leftover_results")
+
+
+
+slices = 28
+share = 14
+leftover = 0
+print(f"share_result")
+print(f"leftover_result")
+print("f...for Mr. Hollow Leg")
